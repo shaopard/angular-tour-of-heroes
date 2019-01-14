768886458d24a2fa589ab671ee3becede26156a9
adrian.buda <adrian.buda@programming-pool.com>
2019-01-14 18:28:59 +0200

    Angular tour of heroes as is on angular.io initial commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+ 
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
+
+
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() 
+  hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.getHero();
+  }
+  
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+  private getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  private goBack(): void {
+    this.location.back();
+  }
+
+}
